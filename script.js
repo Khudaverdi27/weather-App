2a35f6f519e67a70cb39b3a829710ce8d8cc65bc
Khudaverdi Shiralizade <xudaverdi.sirelizade@gmail.com>
2023-10-18 17:40:14 +0400

    some fixes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputUser = document.querySelector(".inputUser");
 const country = document.querySelector(".country");
 const city = document.querySelector(".city");
+const skyCoundation = document.querySelector(".skyCoundation");
 const changeContent = document.querySelectorAll(".changeContent");
 const url = "https://api.openweathermap.org/data/2.5/";
 const key = "1882ca3053a86bff2e348a30b9d66d62";
@@ -8,6 +9,7 @@ const key = "1882ca3053a86bff2e348a30b9d66d62";
 const setQuery = (e) => {
     if (e.keyCode === 13) {
         getResult(inputUser.value);
+        // test(inputUser.value)
         e.preventDefault()
     }
 };
@@ -15,7 +17,9 @@ const setQuery = (e) => {
 inputUser.addEventListener("keypress", setQuery);
 
 const getResult = async (cityName) => {
-    const query = `${url}weather?q=${cityName}&appid=${key}&lang=az`;
+    const query = `${url}weather?q=${cityName}&appid=${key}&lang=en`;
+
+
 
     try {
         const response = await fetch(query);
@@ -29,19 +33,51 @@ const getResult = async (cityName) => {
         console.error("error: ", error);
     }
 };
-
+let sunriseSetArr = []
 const displayResult = (result) => {
     country.textContent = result.sys.country
     city.textContent = result.name + ","
     console.log(result);
+    changetUNIX(result.sys.sunrise, result.sys.sunset)
     for (let i = 0; i < changeContent.length; i++) {
         changeContent[0].textContent = result.wind.speed + "m/s"
         changeContent[1].textContent = result.main.humidity + "%"
         changeContent[3].textContent = Math.round(result.main.temp - 272.15) + "°C"
         changeContent[4].textContent = Math.round(result.main.feels_like - 272.15)
-        changeContent[6].textContent = (result.visibility) / 1000 + "km"
+        changeContent[6].textContent = Math.round((result.visibility) / 1000) + "km"
         changeContent[7].textContent = result.clouds.all + "%"
+        changeContent[9].textContent = sunriseSetArr[sunriseSetArr.length - 2]
+        changeContent[10].textContent = sunriseSetArr[sunriseSetArr.length - 1]
+        if (result.clouds.all >= 50 && result.clouds.all <= 75) {
+            changeContent[8].textContent = 55 + "%"
+        } else if (result.clouds.all >= 75) {
+            changeContent[8].textContent = 100 + "%"
+        } else {
+            changeContent[8].textContent = 0 + "%"
 
+        }
     }
-    console.log(changeContent);
-};
+
+    skyCoundation.textContent = result.weather[0].description
+}
+console.log(changeContent);
+
+//change to UNIX timestamp to normal timestamp
+function changetUNIX(unixSunrise, unixSunset) {
+    let date = new Date(unixSunrise * 1000)
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+
+    let dateSunset = new Date(unixSunset * 1000)
+    let hoursSunset = dateSunset.getHours();
+    let minutesSunset = dateSunset.getMinutes();
+
+    sunriseSetArr.push(hours + ":" + minutes, hoursSunset + ":" + minutesSunset);
+}
+
+// async function test(valu) {
+//     let a = await (await fetch(`http://worldtimeapi.org/api/timezone${valu}`)).json()
+//     console.log(a);
+// }
+
+
